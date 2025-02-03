a52dea9418ab2ca52b6a9b78535861eb9f847ddb
Yevgeniya Kharchenko <yevgeniya.kharchenko@gmail.com>
2025-02-03 22:57:58 +0500

    Обновлена структура URL

--- helpers/urls.py
+++ helpers/urls.py
@@ -1,7 +1,8 @@
 class Urls:
+    MAIN_PAGE = 'https://qa-scooter.praktikum-services.ru/'
+    ORDER_PAGE = f'{MAIN_PAGE}order'
+    ORDER_STATUS_PAGE = f'{MAIN_PAGE}track'
+
     DZEN_HOME_PAGE = 'https://dzen.ru/?yredirect=true'
     YANDEX_HOME_PAGE = 'https://yandex.ru'
     YANDEX_PASSPORT = 'https://sso.passport.yandex.ru'
-    MAIN_PAGE = 'https://qa-scooter.praktikum-services.ru/'
-    ORDER_PAGE = 'https://qa-scooter.praktikum-services.ru/order'
-    ORDER_STATUS_PAGE = 'https://qa-scooter.praktikum-services.ru/track'
